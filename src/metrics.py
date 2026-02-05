ecabb1265811d0506ac411cd50d31fbcba79b7fc
matltin <ma1383ahmadi@gmail.com>
2026-02-05 12:00:00 +0330

    refact(src): remove unneccessary files

--- src/metrics.py
+++
@@ -1,7 +0,0 @@
-from sklearn.metrics import f1_score
-
-
-def f1(y_true, y_pred, average: str = "macro", positive_label: str = "Work"):
-    if average == "binary":
-        return f1_score(y_true, y_pred, average="binary", pos_label=positive_label)
-    return f1_score(y_true, y_pred, average=average)
